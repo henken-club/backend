c345f007d06ed15b143def3abc1f3c46f6c091f2
SnO₂WMaN <me@sno2wman.net>
2021-10-17 07:29:27 +0900

    feat(graphql-content-service): 移管 (#20)

---
+++ graphql-services/content/src/contents/entities/books.entities.ts
@@ -0,0 +1,63 @@
+import {
+  Directive,
+  Field,
+  ID,
+  InputType,
+  Int,
+  ObjectType,
+  registerEnumType,
+} from "@nestjs/graphql";
+
+import {
+  Connection,
+  Edge,
+  Node,
+  OrderDirection,
+  PageInfoEntity,
+} from "~/pagination/pagination.types";
+
+@ObjectType("Book", { implements: () => [Node] })
+@Directive("@key(fields: \"id\")")
+export class BookEntity implements Node {
+  @Field((type) => ID)
+  id!: string;
+
+  @Field((type) => String)
+  title!: string;
+}
+
+@ObjectType("BookEdge", { implements: () => [Edge] })
+export class BookEdgeEntity implements Edge {
+  @Field((type) => String)
+  cursor!: string;
+
+  node!: { id: string };
+}
+
+@ObjectType("BookConnection", { implements: () => [Connection] })
+export class BookConnectionEntity implements Connection {
+  @Field((type) => [BookEdgeEntity])
+  edges!: BookEdgeEntity[];
+
+  @Field((type) => PageInfoEntity)
+  pageInfo!: PageInfoEntity;
+
+  @Field(() => Int)
+  totalCount!: number;
+}
+
+export enum BookOrderField {
+  ID,
+}
+registerEnumType(BookOrderField, {
+  name: "BookOrderField",
+});
+
+@InputType()
+export class BookOrder {
+  @Field((type) => OrderDirection)
+  direction!: OrderDirection;
+
+  @Field((type) => BookOrderField)
+  field!: BookOrderField;
+}
