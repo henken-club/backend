9f2cedcaf1848b4e460c76ffb17672761d86d194
SnO₂WMaN <me@sno2wman.net>
2021-10-17 18:43:34 +0900

    feat(grpc-bookcover): 移管 (#28)

---
+++ grpc-services/bookcover/src/bookcover.ts
@@ -0,0 +1,22 @@
+import got from "got";
+
+import { proxy } from "./imgproxy";
+import { BookcoverServer } from "./protogen/bookcover";
+
+export const openbd = (isbn: string) =>
+  got
+    .get("https://api.openbd.jp/v1/get", { searchParams: { isbn } })
+    .json<[null | { summary: { cover?: string } }]>()
+    .then((result) => result[0]?.summary.cover);
+
+export const findFromISBN: BookcoverServer["findFromISBN"] = async (
+  { request: { isbn } },
+  callback,
+) => {
+  const url = await Promise.any([openbd(isbn)]);
+  return callback(null, url ? { url: proxy(url) } : {});
+};
+
+export const impl: BookcoverServer = {
+  findFromISBN,
+};
---
+++ grpc-services/bookcover/src/main.ts
@@ -0,0 +1,19 @@
+import { Server, ServerCredentials } from "@grpc/grpc-js";
+
+import { impl as bookcoverServer } from "./bookcover";
+import { BookcoverService } from "./protogen/bookcover";
+
+function main() {
+  const server = new Server();
+
+  server.addService(BookcoverService, bookcoverServer);
+  server.bindAsync(
+    // eslint-disable-next-line no-process-env
+    `0.0.0.0:${process.env.PORT || 5000}`,
+    ServerCredentials.createInsecure(),
+    () => {
+      server.start();
+    },
+  );
+}
+main();
