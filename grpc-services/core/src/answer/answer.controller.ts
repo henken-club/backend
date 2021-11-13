e27e956a9f6671aeb68f9b4209f609fa91107bb8
SnO₂WMaN <me@sno2wman.net>
2021-11-13 21:19:40 +0900

    feat: grpc-coreのanswerの実装 (#47)

--- grpc-services/core/src/answer/answer.controller.ts
+++ grpc-services/core/src/answer/answer.controller.ts
@@ -1,7 +1,8 @@
-import { Metadata } from "@grpc/grpc-js";
 import { Controller } from "@nestjs/common";
-import { Observable } from "rxjs";
 
+import { AnswersService } from "./answer.service";
+
+import { HenkensService } from "~/henken/henken.service";
 import {
   AnswerController as IAnswerController,
   AnswerControllerMethods,
@@ -13,30 +14,85 @@ import {
   GetAnswerResponse,
   HENKENCLUB_CORE_PACKAGE_NAME,
 } from "~/protogen/core/answer";
+import { Code } from "~/protogen/google/rpc/code";
 
 @AnswerControllerMethods()
 @Controller(HENKENCLUB_CORE_PACKAGE_NAME)
 export class AnswerController implements IAnswerController {
-  constructor() {}
+  constructor(
+    private readonly answers: AnswersService,
+    private readonly henkens: HenkensService,
+  ) {}
 
-  getAnswer(
-    request: GetAnswerRequest,
-    metadata?: Metadata,
-  ): Observable<GetAnswerResponse> {
-    throw new Error("Method not implemented.");
+  async getAnswer({ id }: GetAnswerRequest): Promise<GetAnswerResponse> {
+    return this.answers.getById(id)
+      .then((answer) => {
+        return (
+          {
+            status: { code: Code.OK, message: "Got answer successfully" },
+            answer: this.answers.convertToGrpcEntity(answer),
+          }
+        );
+      }).catch(() => (
+        {
+          status: { code: Code.NOT_FOUND, message: "Answer not found" },
+          answer: undefined,
+        }
+      ));
   }
 
-  findAnswer(
-    request: FindAnswerRequest,
-    metadata?: Metadata,
-  ): Observable<FindAnswerResponse> {
-    throw new Error("Method not implemented.");
+  async findAnswer(
+    { query }: FindAnswerRequest,
+  ): Promise<FindAnswerResponse> {
+    if (query && query.$case === "id") {
+      return this.answers.findById(query.id)
+        .then((answer) => {
+          return (
+            {
+              status: { code: Code.OK, message: "Find answer successfully" },
+              answer: answer
+                ? this.answers.convertToGrpcEntity(answer)
+                : undefined,
+            }
+          );
+        });
+    } else {
+      return {
+        status: {
+          code: Code.INVALID_ARGUMENT,
+          message: "Required id for query",
+        },
+        answer: undefined,
+      };
+    }
   }
 
-  createAnswer(
-    request: CreateAnswerRequest,
-    metadata?: Metadata,
-  ): Observable<CreateAnswerResponse> {
-    throw new Error("Method not implemented.");
+  async createAnswer(
+    { comment, henkenId, type }: CreateAnswerRequest,
+  ): Promise<CreateAnswerResponse> {
+    if (!await this.henkens.getById(henkenId)) {
+      return ({
+        status: {
+          code: Code.INVALID_ARGUMENT,
+          message: "Specified henken does not exist",
+        },
+        answer: undefined,
+      });
+    }
+    return this.answers.create({
+      comment,
+      henkenId,
+      type: this.answers.convertFromGrpcAnswerType(type),
+    }).then(
+      (answer) => ({
+        status: { code: Code.OK, message: "Created answer successfully" },
+        answer: this.answers.convertToGrpcEntity(answer),
+      }),
+    ).catch(
+      () => ({
+        status: { code: Code.INTERNAL, message: "Internal error" },
+        answer: undefined,
+      }),
+    );
   }
 }
--- grpc-services/core/src/answer/answer.module.ts
+++ grpc-services/core/src/answer/answer.module.ts
@@ -1,11 +1,14 @@
 import { Module } from "@nestjs/common";
 
 import { AnswerController } from "./answer.controller";
+import { AnswersService } from "./answer.service";
+
+import { HenkenModule } from "~/henken/henken.module";
 
 @Module({
-  imports: [],
+  imports: [HenkenModule],
   controllers: [AnswerController],
-  providers: [],
-  exports: [],
+  providers: [AnswersService],
+  exports: [AnswersService],
 })
 export class AnswerModule {}
