2f6fecc010bafcd404fc4e0578961a8081f87b1f
SnO2WMaN <me@sno2wman.net>
2021-11-13 23:13:08 +0900

    fix(grpc-core): module周り修正

--- grpc-services/core/src/answer/answer.module.ts
+++ grpc-services/core/src/answer/answer.module.ts
@@ -4,9 +4,10 @@ import { AnswerController } from "./answer.controller";
 import { AnswersService } from "./answer.service";
 
 import { HenkenModule } from "~/henken/henken.module";
+import { PrismaModule } from "~/prisma/prisma.module";
 
 @Module({
-  imports: [HenkenModule],
+  imports: [PrismaModule, HenkenModule],
   controllers: [AnswerController],
   providers: [AnswersService],
   exports: [AnswersService],
